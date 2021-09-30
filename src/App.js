823854edd9a4b6e12dc2e60985598fbc0b3b2ea0
Bellik888 <Bellik888@gmail.com>
2021-09-30 07:36:01 +0300

    test for lint

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import './App.css'
+import './App.css';
+import { Profile } from './components/Profile';
+import user from './json/user.json';
 
 function App() {
   return (
     <div className="App">
+      <Profile
+        avatar={user.avatar}
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        stats={user.stats}
+      />
     </div>
   );
 }
